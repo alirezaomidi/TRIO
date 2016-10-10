dc5215d7bd4439308411072d4a803d63ecbe9ce2
mehran <mehranagh200@gmail.com>
2016-10-10 11:45:01 +0330

    floyd warshal and minimum cut alg added

---
+++ cheat-sheet/Graph/floyd_warshal_All_pairs_shortes_path_and_vairants_O(V^3).cpp
@@ -0,0 +1,50 @@
+// floyd warshal alg is used for findign shortest path between any two verteces, feasible for V <= 400.
+
+
+// inside int main()
+// let p be a 2D parent matrix, where p[i][j] is the last vertex before j
+// on a shortest path from i to j, i.e. i -> ... -> p[i][j] -> j
+
+for (int i = 0; i < n; i++)
+   for (int j = 0; j < n; j++)
+        p[i][j] = i; // initialize the parent matrix
+
+for (int k = 0; k < n; k++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) // this time, we need to use if statement
+            if (AdjMat[i][k] + AdjMat[k][j] < AdjMat[i][j]) {
+                AdjMat[i][j] = AdjMat[i][k] + AdjMat[k][j];
+                p[i][j] = p[k][j];
+            }
+
+
+
+// this func is used to print the shortest path using p vector after running the alg.
+void printPath(int i, int j) {
+    if (i != j) printPath(i, p[i][j]);
+    printf(" %d", j);
+}
+
+// VARIANTS :
+
+
+// 1. findig circle (cheapest circle or detecting negative circle):
+// just set AdjMat[i][i] = inf before running the alg, after running the alg if AdjMat[i][i] is no
+// longer inf then there is a circle, if it is negative then there is a negative circle in graph
+// to find cheapest circle check all adjMat[i][i] for the minimum one.
+
+
+// 2.transitive closure.
+// to see if there is any path from i to j transitive closure alg is used.
+for (int k = 0; k < V; k++)
+	for (int i = 0; i < V; i++)
+		for (int j = 0; j < V; j++)
+			AdjMat[i][j] |= (AdjMat[i][k] & AdjMat[k][j]);
+
+
+// 3.Minmax
+// just replace + operator with max operator --> max(AdjMat[i][k], AdjMat[k][j])
+
+
+// 4. check if i and j belong to the same SCC, after finding transitive closure, if AdjMat[i][j] && AdjMat[j][i]
+// is true then i and j belong to the same SCC.
