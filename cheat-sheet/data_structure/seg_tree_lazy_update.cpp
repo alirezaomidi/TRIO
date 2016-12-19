8da01e52858298c965f9fc06af5eaac2ec22e2a3
mehran <mehranagh200@gmail.com>
2016-12-20 01:10:18 +0330

    minimum cut using max flow algorithm added

--- cheat-sheet/data_structure/seg_tree_lazy_update.cpp
+++ cheat-sheet/data_structure/seg_tree_lazy_update.cpp
@@ -25,7 +25,7 @@ void update_tree(int node, int a, int b, int i, int j, int value) { // update wi
 
 		if(a != b) {
 			lazy[node*2] += lazy[node]; // Mark child as lazy
-    			lazy[node*2+1] += lazy[node]; // Mark child as lazy
+    		lazy[node*2+1] += lazy[node]; // Mark child as lazy
 		}
 
    		lazy[node] = 0; // Reset it
